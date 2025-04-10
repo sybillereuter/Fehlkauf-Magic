8335c9883324a327bdd88cfbfaa67a3e6f80bdab
sybillereuter <sybille.reuter.berlin@gmail.com>
2025-04-10 17:22:55 +0200

    cleaned up FehlkaufTest

--- src/test/java/org/bille/FehlkaufTest.java
+++ src/test/java/org/bille/FehlkaufTest.java
@@ -1,13 +1,14 @@
 package org.bille;
 
 import com.opencsv.exceptions.CsvException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FehlkaufTest {
 
@@ -16,38 +17,10 @@ public class FehlkaufTest {
 
         List<MemberData> data = readWithFehlKaufReader("members.csv");
         MemberData first = data.get(0);
-        Assertions.assertEquals(first.getUserName(), "miau123");
-        Assertions.assertEquals(first.getAddress(), "Mieze Katz\nMiau Str. 12\n12345 Berlin");
-        Assertions.assertFalse(first.isMax());
-        Assertions.assertEquals(first.getCards(), 3);
-    }
-
-    @Test
-    public void run() throws IOException, CsvException {
-
-        List<MemberData> members = readWithFehlKaufReader("Fehlkauf100.csv");
-        System.out.println("read...");
-        MemberMatcher matcher = new MemberMatcher(members);
-        System.out.println("matcher...");
-        FehlkaufRound fehlkaufRound = matcher.match();
-        System.out.println("matched!");
-        int reduction = 0;
-        while (fehlkaufRound == null) {  // handle max!!
-            matcher = new MemberMatcher(members, reduction+=1);
-            fehlkaufRound = matcher.match();
-        }
-        System.out.printf("Diese Runde werden %d Karten verschickt!", fehlkaufRound.getTotalCards());
-        FehlkaufFileUtils.write(
-                fehlkaufRound,
-                new File("Fehlkauf100-liste.txt"), FehlkaufFileUtils.READABLE);
-        FehlkaufFileUtils.write(
-                fehlkaufRound,
-                new File("Fehlkauf100-forum.txt"),FehlkaufFileUtils.PC_FORMAT);
-        FehlkaufFileUtils.write(fehlkaufRound,
-                new File("Fehlkauf100-forum-receivers.txt"), FehlkaufFileUtils.PC_RECEIVERS);
-        FehlkaufFileUtils.write(fehlkaufRound,
-                new File("Fehlkauf100-übersicht.txt"), FehlkaufFileUtils.OVERVIEW);
-
+        assertEquals(first.getUserName(), "miau123");
+        assertEquals(first.getAddress(), "Mieze Katz, Miau Str. 12, 12345 Berlin");
+        assertFalse(first.isMax());
+        assertEquals(first.getCards(), 3);
     }
 
     @Test
@@ -56,12 +29,7 @@ public class FehlkaufTest {
         List<MemberData> members = readWithFehlKaufReader("members.csv");
         MemberMatcher matcher = new MemberMatcher(members);
         FehlkaufRound fehlkaufRound = matcher.match();
-        int reduction = 0;
-        while (fehlkaufRound == null) {  // handle max!!
-            matcher = new MemberMatcher(members, reduction+=1);
-            fehlkaufRound = matcher.match();
-        }
-        System.out.println(FehlkaufFileUtils.getOverview(fehlkaufRound));
+        assertFalse(FehlkaufFileUtils.getOverview(fehlkaufRound).isEmpty());
     }
 
 
