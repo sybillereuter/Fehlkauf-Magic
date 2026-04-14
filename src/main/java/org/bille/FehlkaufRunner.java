d17b9b769d693e837ad7141807535de62d450f84
sybillereuter <sybille.reuter.berlin@gmail.com>
2026-04-14 11:04:54 +0200

    adapted FehlkaufRunner to return 3 separate forum-format files

--- src/main/java/org/bille/FehlkaufRunner.java
+++ src/main/java/org/bille/FehlkaufRunner.java
@@ -48,8 +48,8 @@ public class FehlkaufRunner {
         System.out.printf("Diese Runde werden %d Karten verschickt!%n", fehlkaufRound.getTotalCards());
 
         writeOutputFile(workingDir, fehlkaufRound, round, "Fehlkauf%s-liste.txt", FehlkaufFileUtils.READABLE);
-        writeOutputFile(workingDir, fehlkaufRound, round, "Fehlkauf%s-forum.txt", FehlkaufFileUtils.PC_FORMAT);
-        writeOutputFile(workingDir, fehlkaufRound, round, "Fehlkauf%s-forum-receivers.txt", FehlkaufFileUtils.PC_RECEIVERS);
+        writeOutputFiles(workingDir, fehlkaufRound, round, "Fehlkauf%s-forum.txt", FehlkaufFileUtils.PC_FORMAT);
+        writeOutputFiles(workingDir, fehlkaufRound, round, "Fehlkauf%s-forum-receivers.txt", FehlkaufFileUtils.PC_RECEIVERS);
         writeOutputFile(workingDir, fehlkaufRound, round, "Fehlkauf%s-übersicht.txt", FehlkaufFileUtils.OVERVIEW);
         writeOutputFile(workingDir, fehlkaufRound, round, "Fehlkauf%s-message-receivers.txt", FehlkaufFileUtils.MESSAGE_TO);
 
@@ -62,5 +62,17 @@ public class FehlkaufRunner {
         File outputFile = new File(workingDir, filename);
         FehlkaufFileUtils.write(fehlkaufRound, outputFile, format);
         System.out.println("Erstellt: " + outputFile.getAbsolutePath());
+    }
+
+    private void writeOutputFiles(String workingDir, FehlkaufRound fehlkaufRound, String round, String filenamePattern, String format)
+            throws IOException {
+        String filename = String.format(filenamePattern, round);
+        File outputFile = new File(workingDir, filename);
+        FehlkaufFileUtils.write(fehlkaufRound, outputFile, format);
+        String baseName = filename.substring(0, filename.lastIndexOf('.'));
+        String ext = filename.substring(filename.lastIndexOf('.'));
+        System.out.println("Erstellt: " + new File(workingDir, baseName + "_a-f" + ext).getAbsolutePath());
+        System.out.println("Erstellt: " + new File(workingDir, baseName + "_g-r" + ext).getAbsolutePath());
+        System.out.println("Erstellt: " + new File(workingDir, baseName + "_s-z" + ext).getAbsolutePath());
     }
 }
